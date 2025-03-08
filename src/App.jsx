2b951d5d07a26ca22f524d883932ac915ea232df
Timmydrax <timmydrax@gmail.com>
2025-03-08 22:11:16 +0100

    NEW: Added a new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,9 +18,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          <h2>{count}</h2>
+        </div>
+        <div className="btn">
+          <button onClick={() => setCount((count) => count + 1)}>
+            Increment
+          </button>
+          <button onClick={() => setCount((count) => count - 1)}>
+            Decrement
+          </button>
+        </div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
